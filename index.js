d12a5438d991e61e5d5adab11adee5daeaa3d200
Ritika Malik <ritikamalik100102@gmail.com>
2022-12-20 04:04:16 +0530

    Prompt and alerts

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (team1 == null || team1 == "") {
     team_1.innerText = team1
 }
 
-let team2 = prompt("Please enter the name of team 2","TEAM - 2");
+let team2 = prompt("Please enter the name of Team - 2","TEAM - 2");
 if(team2 == null || team2 == "") {
     team_2.innerText = "TEAM 2"
 }
@@ -21,16 +21,16 @@ else {
 
 
 
-let home_count = 0
-let guest_count = 0
+let team1_count = 0
+let team2_count = 0
 let count = 0
 check()
 
 
 function end_id() {
-    if(home_count == guest_count )
+    if(team1_count == team2_count )
     alert("THERE IS A TIE!");
-    else if(home_count > guest_count)
+    else if(team1_count > team2_count)
     alert( team_1.innerText + " IS THE WINNER!")
     else
     alert ( team_2.innerText + " IS THE WINNER!")
@@ -39,69 +39,69 @@ function end_id() {
 }
 
 function check() {
-    if(home_count == 0 && guest_count == 0  && count == 0 ){
+    if(team1_count == 0 && team2_count == 0  && count == 0 ){
         lead.textContent = "LET THE MATCH BEGIN!"
     }
-    else if(home_count == guest_count){
+    else if(team1_count == team2_count){
         lead.textContent = "LEADING: Tie "
     }
-    if(home_count > guest_count){
-        result = home_count - guest_count
-        lead.textContent = "LEADING: Team " + team_1.innerText + " by " + result + " run "
+    if(team1_count > team2_count){
+        result = team1_count - team2_count
+        lead.textContent = "LEADING: Team " + team_1.innerText + " is leading by " + result + " run(s) "
     }
     
-    if(guest_count > home_count){
-        result = guest_count - home_count
-        lead.textContent = "LEADING: Team " + team_2.innerText + " by " + result + " run " 
+    if(team2_count > team1_count){
+        result = team2_count - team1_count
+        lead.textContent = "LEADING: Team " + team_2.innerText + " is leading by " + result + " run(s) " 
     }
 }
 
 
 function home_1() {
-    home_count += 1
-    home_el.innerText = home_count
+    team1_count += 1
+    home_el.innerText = team1_count
     count++
     check()
 }
 
 function home_2() {
-    home_count += 2
-    home_el.innerText = home_count
+    team1_count += 2
+    home_el.innerText = team1_count
     count++
     check()
 }
 
 function home_3() {
-    home_count += 3
-    home_el.innerText = home_count
+    team1_count += 3
+    home_el.innerText = team1_count
     count++
     check()
 }
 
 function guest_1() {
-    guest_count += 1
-    guest_el.innerText = guest_count
+    team2_count += 1
+    guest_el.innerText = team2_count
     count++
     check()
 }
 
 function guest_2() {
-    guest_count += 2
-    guest_el.innerText = guest_count
+    team2_count += 2
+    guest_el.innerText = team2_count
     count++
     check()
 }
 
 function guest_3() {
-    guest_count += 3
-    guest_el.innerText = guest_count
+    team2_count += 3
+    guest_el.innerText = team2_count
     count++
     check()
 }
 
 function new_id() {
-    home_count = 0
-    guest_count = 0
+    team1_count = 0
+    team2_count = 0
     home_el.innerText = 0
     guest_el.innerText = 0
     count = 0
@@ -109,52 +109,52 @@ function new_id() {
 }
 
 function home_r() {
-    home_count = 0
-    home_el.innerText = home_count
+    team1_count = 0
+    home_el.innerText = team1_count
     count++
     check()
 }
 
 function guest_r() {
-    guest_count = 0
-    guest_el.innerText = guest_count
+    team2_count = 0
+    guest_el.innerText = team2_count
     count++
     check()
 }
 
 function home__1() {
-    home_count +=-1
-    home_el.innerText = home_count
+    team1_count +=-1
+    home_el.innerText = team1_count
     count++
     check()
 }
 function home__2() {
-    home_count +=-2
-    home_el.innerText = home_count
+    team1_count +=-2
+    home_el.innerText = team1_count
     count++
     check()
 }
 function home__3() {
-    home_count +=-3
-    home_el.innerText = home_count
+    team1_count +=-3
+    home_el.innerText = team1_count
     count++
     check()
 }
 function guest__1() {
-    guest_count += -1
-    guest_el.innerText = guest_count
+    team2_count += -1
+    guest_el.innerText = team2_count
     count++
     check()
 }
 function guest__2() {
-    guest_count += -2
-    guest_el.innerText = guest_count
+    team2_count += -2
+    guest_el.innerText = team2_count
     count++
     check()
 }
 function guest__3() {
-    guest_count += -3
-    guest_el.innerText = guest_count
+    team2_count += -3
+    guest_el.innerText = team2_count
     count++
     check()
 }
